3575dc22da3f1e32aee3333c065270d2e428e1b2
Alex <rabbit_1399@icloud.com>
2022-10-20 07:53:05 +0300

    fix: adding more rules for /reg + adding editing birthday in bot

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Happy Birthday bot:
+# Happy Birthday bot
+
+# Usage:
+Add a bot to your chat and just start using!
+
+### Commands:
+    /reg - Registraion user, has 1 optional argument `date` in format like dd/mm/yyyy | dd.mm.yyyy | dd-mm-yyyy
+    /info - Returns info about users and their birthdays
+    /fullnes - Returns count users in chat and users in cache with birthdays
+    /test - Returns test happy birthday message
+    /help - help message
--- src/commands.py
+++ src/commands.py
@@ -2,36 +2,72 @@ import re
 from datetime import datetime, date
 from typing import Any
 
-from telegram import Update
-from telegram.ext import ContextTypes, CommandHandler, CallbackContext
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import ContextTypes, CommandHandler, CallbackContext, CallbackQueryHandler
 
 from dependency_injector.wiring import inject, Provide
+from loguru import logger
 
 from container import Container
 from src.birthday_utils import create_birthday_message
 from src.entities import User
+from src.redis_cache import Cache
 
 
 def _to_datetime(dt: str | None) -> date | None:
     if dt is None:
         return None
-
     splitter = re.search(r'[./-]', dt).group()
     fmt = '%d{0}%m{0}%Y'.format(splitter)
     return datetime.strptime(dt, fmt).date()
 
 
 @inject
+async def _callback_reg_query(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE,
+    cache: Cache = Provide[Container.cache]
+) -> None:
+    query = update.callback_query
+    await query.answer()
+    choice = query.data
+
+    if choice == '0':
+        await context.bot.edit_message_text(
+            chat_id=query.message.chat_id,
+            message_id=query.message.message_id,
+            text='Ваши данные остались теми же.',
+            parse_mode='HTML'
+        )
+    elif choice == '1':
+        key = (update.effective_user.id, update.effective_chat.id)
+        dt = context.user_data[key]
+        u = await cache.get(key)
+        u.birthday = dt
+        await cache.set(u)
+        await context.bot.edit_message_text(
+            chat_id=query.message.chat_id,
+            message_id=query.message.message_id,
+            text=u.__str__(),
+            parse_mode='HTML'
+        )
+    else:
+        logger.error("Bad callback")
+
+
+@inject
 async def _reg(
     update: Update,
     context: ContextTypes.DEFAULT_TYPE,
-    cache = Provide[Container.cache]
+    cache: Cache = Provide[Container.cache]
 ) -> None:
     """
     Registration user with Optional argument for birthday
 
     using:
         /reg            - save without birthday
+
+        /reg {date_arg: str | optional}
 
         ### format mm{./-}dd{./-}YYYY
         /reg 01.01.2001 - save with birthday
@@ -42,22 +78,38 @@ async def _reg(
     :param context:
     :return:
     """
-    # ToDo: заполнять недостающую информацию о пользователе
     key = (update.effective_user.id, update.effective_chat.id)
     user = await cache.get(key)
-    try:
-        dt = re.findall(r'\d{2}[./-]\d{2}[./-]\d{4}', context.args[0])[0] if context.args else None
-    except IndexError:
+    dt = re.findall(r'\d{2}[./-]\d{2}[./-]\d{4}', context.args[0]) if context.args else None
+
+    if (context.args and len(context.args) > 1) or (context.args and not dt):
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text='Уважаемый <i>{}</i> вводите валидный формат данных или идите нахуй'.format(
+            text='Уважаемый <i>{}</i> вводите валидный формат данных'.format(
                 update.effective_user.name),
             parse_mode='HTML'
         )
         return
-
-    if user and not user.birthday and dt:
-        user.birthday = _to_datetime(dt)
+    if user and dt and dt != user.birthday:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='Вы {0} уверены, что хотите заменить {1} на {2} дату?'.format(
+                user.name,
+                user.birthday.strftime('%d.%m.%Y'),
+                dt[0]
+            ),
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[
+                    [
+                        InlineKeyboardButton("Да", callback_data='1'),
+                        InlineKeyboardButton("Нет", callback_data='0')
+                    ]
+                ],
+                one_time_keyboard=True
+            )
+        )
+        context.user_data[key] = _to_datetime(dt[0])
+        return
     elif user and not dt and user.birthday:
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
@@ -72,15 +124,15 @@ async def _reg(
             name=update.effective_user.name,
             birthday=_to_datetime(dt)
         )
-    await cache.set(user)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=user.__str__(), parse_mode='HTML')
+        await cache.set(user)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=user.__str__(), parse_mode='HTML')
 
 
 @inject
 async def _users_info(
     update: Update,
     context: ContextTypes.DEFAULT_TYPE,
-    cache = Provide[Container.cache]
+    cache: Cache = Provide[Container.cache]
 ) -> None:
     """
     Returns info about user and their birthdays
@@ -93,7 +145,6 @@ async def _users_info(
     :param context:
     :return:
     """
-    # ToDo: select with group chat id
     data = await cache.all_chat_str(update.effective_chat.id)
     if not data:
         text = 'No data about users in this chat'
@@ -111,7 +162,7 @@ async def _users_info(
 async def _fullness_check(
     update: Update,
     context: ContextTypes.DEFAULT_TYPE,
-    cache = Provide[Container.cache]
+    cache: Cache = Provide[Container.cache]
 ) -> None:
     """
     Check that count users in chat equals users in cache
@@ -131,7 +182,7 @@ async def _fullness_check(
 async def _help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     text = " The following commands are available:\n"
     commands = [
-        ["/reg", "Registration user with Optional argument for birthday,\n     like: /reg 01.01.2001"],
+        ["/reg", "Registration user with Optional argument for birthday,\n{like:15s}: /reg 01.01.2001"],
         ["/info", "Returns info about user and their birthdays"],
         ["/fullness", "Check that count users in chat equals users in cache"],
         ["/help", "Get this message"]
@@ -146,7 +197,7 @@ async def _help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def _test_try(
     update: Update,
     context: ContextTypes.DEFAULT_TYPE,
-    cache = Provide[Container.cache]
+    cache: Cache = Provide[Container.cache]
 ) -> None:
     key = (update.effective_user.id, update.effective_chat.id)
     user = await cache.get(key)
@@ -166,5 +217,6 @@ def get_handlers() -> list[CommandHandler[CallbackContext | Any]]:
         CommandHandler('info', _users_info),
         CommandHandler('fullness', _fullness_check),
         CommandHandler('test', _test_try),
-        CommandHandler('help', _help)
+        CommandHandler('help', _help),
+        CallbackQueryHandler(_callback_reg_query)
     ]
