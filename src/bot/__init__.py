96e3bcf9d64675e85cca5355feb80d7e79f99ec3
Alex <rabbit_1399@icloud.com>
2023-03-29 13:03:19 +0300

    fix: birthdays handler message

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -3,7 +3,7 @@ from typing import Any
 from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filters
 
 from src.bot.handlers.help import help_handler
-from src.bot.handlers.info import birthdays
+from src.bot.handlers.birthdays import birthdays_handler
 from src.bot.handlers.join_chat import new_member
 from src.bot.handlers.nearest import nearest_handler
 from src.bot.handlers.reg import reg_handler
@@ -14,7 +14,7 @@ def get_handlers() -> list[CommandHandler[CallbackContext | Any]]:
     return [
         CommandHandler('reg', reg_handler),
         CommandHandler('nearest', nearest_handler),
-        CommandHandler('birthdays', birthdays),
+        CommandHandler('birthdays', birthdays_handler),
         MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, new_member),
 
         CommandHandler('help', help_handler),
--- src/bot/handlers/birthdays.py
+++ src/bot/handlers/birthdays.py
@@ -9,7 +9,7 @@ from src.dao.storage.cache import Cache
 
 
 @inject
-async def birthdays(
+async def birthdays_handler(
     update: Update,
     context: ContextTypes.DEFAULT_TYPE,
     cache: Cache = Provide[Container.cache]
@@ -25,9 +25,9 @@ async def birthdays(
     await cache.update_if_not_exists(tg_chat=update.effective_chat, tg_user=update.effective_user, lazy=True)
     users = await cache.users_by_room(update.effective_chat.id)
     text = "Users with birthdays ({0}/{1}):\n{2}".format(
+        len(users),
         # -1 because bot not user:/
-        len(users) - 1,
-        await update.effective_chat.get_member_count(),
+        await update.effective_chat.get_member_count() - 1,
         "\n".join(["{0:15s} {1}".format(u.name, u.birthday.strftime('%d.%m')) for u in users])
     )
 
